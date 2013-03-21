22ef3d5c71299951055c89b9e5343dc061aadbf5
Steven Haryanto (on PC) <stevenharyanto@gmail.com>
2013-03-21 11:36:21 +0700

    Initial commit

---
+++ lib/Web/Detect.pm
@@ -0,0 +1,60 @@
+package Web::Detect;
+
+use 5.010001;
+use strict;
+use warnings;
+use Log::Any '$log';
+
+# VERSION
+
+require Exporter;
+our @ISA       = qw(Exporter);
+our @EXPORT_OK = qw(detect_web);
+
+sub detect_web {
+    my %res;
+
+    if ($ENV{GATEWAY_INTERFACE} =~ m!^CGI/!) {
+        $res{cgi} = 1;
+    }
+    if ($ENV{MOD_PERL}) {
+        $res{mod_perl} = 1;
+    }
+    if ($ENV{PLACK_ENV}) {
+        $res{plack} = 1;
+        $res{psgi} = 1;
+    }
+
+    return undef unless %res;
+    \%res;
+}
+
+1;
+#ABSTRACT: Detect if program is running under some web environment
+
+=head1 SYNOPSIS
+
+ use Web::Detect qw(detect_web);
+ say "Running under terminal" if detect_web();
+
+
+=head1 DESCRIPTION
+
+
+=head1 FUNCTIONS
+
+=head2 detect_web() => HASHREF
+
+Return undef if not detected running under any web environment.
+
+Return a hash otherwise, with following keys: C<mod_perl> (bool, true if
+detected running under mod_perl), C<plack> (bool, true if detected running under
+Plack), C<cgi> (bool, true if detected running under CGI).
+
+
+=head1 TODO
+
+
+=head1 SEE ALSO
+
+=cut
